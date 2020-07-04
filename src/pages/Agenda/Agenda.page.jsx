2792f124b45212418995ea6958e666923166c644
Victor Jordan <sistema.sgo.ccb@gmail.com>
2020-07-04 02:03:53 -0300

    adding calendar component

--- src/pages/Agenda/Agenda.page.jsx
+++ src/pages/Agenda/Agenda.page.jsx
@@ -1,16 +1,52 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { Container, Row, Col } from 'react-bootstrap';
-
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'moment/locale/pt-br';
 import userContext from '../../store/UserContext';
 
 import Header from '../../components/Header';
+
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 const StyledMain = styled.main`
   display: inline-flex;
   height: 100vh;
   width: calc(100vw - 250px);
 `;
+
+const messages = {
+  allDay: 'Dia inteiro',
+  previous: '<',
+  next: '>',
+  today: 'Hoje',
+  month: 'Mês',
+  week: 'Semana',
+  day: 'Dia',
+  agenda: 'Agenda',
+  date: 'Data',
+  time: 'Hora',
+  event: 'Evento',
+};
+
+const events = [
+  {
+    title: 'teste',
+    start: new Date('06/06/2020'),
+    end: new Date('06/06/2020'),
+    allDay: false,
+  },
+  {
+    title: 'teste',
+    start: new Date('06/07/2020'),
+    end: new Date('06/07/2020'),
+    allDay: false,
+  },
+];
+
+moment.locale('pt-br');
+const localizer = momentLocalizer(moment);
 
 function Agenda() {
   const currentlyUser = useContext(userContext);
@@ -20,9 +56,15 @@ function Agenda() {
       <StyledMain>
         <Container fluid>
           <Row>
-            <Col xs={12}>1 of 1</Col>
             <Col xs={12}>
-              <h1>Olha a sua agenda {currentlyUser?.user?.name}</h1>
+              <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 'calc(100vh' }}
+                messages={messages}
+              />
             </Col>
           </Row>
         </Container>
