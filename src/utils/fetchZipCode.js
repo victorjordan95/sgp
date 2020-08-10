5a65c886c2decb3eaa532b68f182d0614ac0b389
Victor Jordan <victorjordan95@gmail.com>
2020-08-10 19:10:55 -0300

    fixing zip

--- src/utils/fetchZipCode.js
+++ src/utils/fetchZipCode.js
@@ -4,20 +4,23 @@ import { toast } from 'react-toastify';
 import stateValues from './brStatesValues';
 
 const fetchZipCode = async zipcode => {
-  let zip;
+  let zip = {};
   try {
     zip = await cep(zipcode);
-    return {
-      zip: zip.cep,
-      city: zip.city,
-      state: stateValues.filter(state => state.value === zip?.state),
-      neighborhood: zip.neighborhood,
-      street: zip.street,
-    };
+    if (zip) {
+      zip = {
+        zip: zip.cep,
+        city: zip.city,
+        state: stateValues.filter(state => state.value === zip?.state),
+        neighborhood: zip.neighborhood,
+        street: zip.street,
+      };
+    }
   } catch (error) {
     toast.error(error);
     return false;
   }
+  return zip;
 };
 
 export default fetchZipCode;
